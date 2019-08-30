e8ef3d26995b080fabf481ace40bc5f9e82b486e
Dirk Wenzel <der-wenzel@gmx.de>
2019-08-30 21:00:24 +0200

    [RELEASE] Release of vcc 4.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
     ),
   ),
   'state' => 'stable',
-  'version' => '4.0.0',
+  'version' => '4.1.0',
   'clearCacheOnLoad' => 0,
   'createDirs' => '',
   'uploadfolder' => 0,
